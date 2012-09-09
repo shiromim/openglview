a17c64f9a70012aa28bd279c7a276e6dfcc89a30
Hiromi Sawada <istbleu@gmail.com>
2012-09-09 15:25:53 +0900

    Initial Commit

---
+++ OpenGLView/OpenGLViewAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OpenGLViewAppDelegate.h
+//  OpenGLView
+//
+//  Created by Hiromi Sawada on 9/9/12.
+//  Copyright (c) 2012 shiromim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLViewAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGLView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGLView
+//
+//  Created by Hiromi Sawada on 9/9/12.
+//  Copyright (c) 2012 shiromim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OpenGLViewAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OpenGLViewAppDelegate class]));
+    }
+}
